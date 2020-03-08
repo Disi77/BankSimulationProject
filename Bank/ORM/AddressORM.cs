808ccd8a79d7ccb0c7629d12f8c4e16e75180111
Petra Číhalová <cihalova.petra@centrum.cz>
2020-03-08 23:12:39 +0100

    In process of updating user, check if address was affected. If so implementation of the scenario of creating new address or find existing address in DB and change this information in Users table.

--- Bank/ORM/AddressORM.cs
+++ Bank/ORM/AddressORM.cs
@@ -14,10 +14,17 @@ namespace Bank.ORM
             "INSERT INTO Address VALUES (@AddressId,@Street,@StreetNumber,@City,@PostalCode,@Country)";
         private static String getAddressWithLastId = 
             "SELECT TOP(1) * FROM address ORDER BY AddressId desc";
+        
         private static String selectByAddressId =
             "SELECT * FROM Address WHERE AddressId=@AddressId";
         private static String selectCountbyAddressId =
             "SELECT count(*) FROM Address WHERE AddressId=@AddressId";
+        
+        private static String isAddressInDb =
+            "SELECT * FROM Address WHERE Street=@Street and StreetNumber=@StreetNumber and City=@City and PostalCode=@PostalCode and Country=@Country";
+        private static String countIsAddressInDb =
+            "SELECT count(*) FROM Address WHERE Street=@Street and StreetNumber=@StreetNumber and City=@City and PostalCode=@PostalCode and Country=@Country";
+
 
         public static bool CreateAddress(Address address)
         {
@@ -81,6 +88,73 @@ namespace Bank.ORM
                 default:
                     MessageBox.Show("More than one Address with same ID in Database. Check your reques.");
                     return null;
+            }
+        }
+
+        public static Address IsAddressInDatabase(Address address)
+        {
+            int count;
+            DBConnection connection = new DBConnection();
+            connection.OpenConection();
+
+            SqlCommand command = connection.CreateCommand(isAddressInDb);
+            command.Parameters.AddWithValue("@Street", address.Street);
+            command.Parameters.AddWithValue("@StreetNumber", address.StreetNumber);
+            command.Parameters.AddWithValue("@City", address.City);
+            command.Parameters.AddWithValue("@PostalCode", address.PostalCode);
+            command.Parameters.AddWithValue("@Country", address.Country);
+
+            SqlCommand commandCount = connection.CreateCommand(countIsAddressInDb);
+            commandCount.Parameters.AddWithValue("@Street", address.Street);
+            commandCount.Parameters.AddWithValue("@StreetNumber", address.StreetNumber);
+            commandCount.Parameters.AddWithValue("@City", address.City);
+            commandCount.Parameters.AddWithValue("@PostalCode", address.PostalCode);
+            commandCount.Parameters.AddWithValue("@Country", address.Country);
+            
+            count = (int)commandCount.ExecuteScalar();
+            SqlDataReader reader = command.ExecuteReader();
+
+            switch (count)
+            {
+                case 0:
+                    connection.CloseConnection();
+                    return null;
+                case 1:
+                    Address newAddress = new Address();
+                    while (reader.Read())
+                    {
+                        newAddress.Id = reader.GetInt32(0);
+                        newAddress.Street = reader.GetString(1);
+                        newAddress.StreetNumber = reader.GetString(2);
+                        newAddress.City = reader.GetString(3);
+                        newAddress.PostalCode = reader.GetString(4);
+                        newAddress.Country = reader.GetString(5);
+                    }
+                    connection.CloseConnection();
+                    return newAddress;
+                default:
+                    List<Address> addressesList = new List<Address>();
+                    while (reader.Read())
+                    {
+                        Address add = new Address();
+                        add.Id = reader.GetInt32(0);
+                        add.Street = reader.GetString(1);
+                        add.StreetNumber = reader.GetString(2);
+                        add.City = reader.GetString(3);
+                        add.PostalCode = reader.GetString(4);
+                        add.Country = reader.GetString(5);
+                        addressesList.Add(add);
+                    }
+
+                    Address addressWithLowestId = addressesList[0];
+                    foreach (Address a in addressesList)
+                    {
+                        if (a.Id < addressWithLowestId.Id)
+                            addressWithLowestId = a;                    
+                    }
+
+                    connection.CloseConnection();
+                    return addressWithLowestId;
             }
         }
 
